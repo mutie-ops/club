495ae175953bf4b27af2513605c91bc6217d366c
Benjamin Mutie Mutisya <benjaminmutie53@gmail.com>
2024-01-28 20:52:52 +0300

    PUSH THIS CHANGES

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import CreateAccout from './pages/createAccount';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   
+     <CreateAccout />  
+   
+     
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- pages/createAccount.jsx
+++ pages/createAccount.jsx
@@ -1,34 +1,40 @@
 import React from "react";
-import {Text, View,SafeAreaView,StyleSheet, ImageBackground} from 'react-native'
-
-
-const background = require('../assets/out-door.png')
+import {Text, View,SafeAreaView,StyleSheet, ImageBackground,StatusBar} from 'react-native'
 
 
 function CreateAccout(){
-
+    const background = require('../assets/out-door.jpg')
+    const Logo = require('../assets/final-logo.png')
 
     const create_account = 
 
-       
-       <ImageBackground src={background}>
-
-       </ImageBackground>
+       <ImageBackground source={background} style={styles.container} blurRadius={4}>
+        
+            <View>
+                <Text>THIS IS THE LOGO</Text>
+            </View>
+            <View>
+                <Image source={Logo}/>
+            </View>
+      
+        </ImageBackground>
         
 
-    return create_account
-
-
-
-    const style = StyleSheet.create({
-
-    })
-
-
-
-    
+    return create_account 
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent:'center',
+        resizeMode:'cover',
+        alignItems:'center'
+        
+    }
+   
+    
+})
 
 
 
