99eca8f833c4aa4f2826cb06f56b3baf4fcb5e0d
Benjamin Mutie Mutisya <benjaminmutie53@gmail.com>
2024-01-28 19:37:06 +0300

    fist comit

---
+++ pages/createAccount.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {Text, View,SafeAreaView,StyleSheet, ImageBackground} from 'react-native'
+
+
+const background = require('../assets/out-door.png')
+
+
+function CreateAccout(){
+
+
+    const create_account = 
+
+       
+       <ImageBackground src={background}>
+
+       </ImageBackground>
+        
+
+    return create_account
+
+
+
+    const style = StyleSheet.create({
+
+    })
+
+
+
+    
+
+}
+
+
+
+
+export default CreateAccout
